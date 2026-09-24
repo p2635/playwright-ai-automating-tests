258e67227b65297f209354bdcba42108cab51da2
Philip Wong <4522927+p2635@users.noreply.github.com>
2026-09-24 16:33:54 +0100

    refactor: update edit-bug-dialog.page.ts with more locators

--- tests/pages/edit-bug-dialog.page.ts
+++ tests/pages/edit-bug-dialog.page.ts
@@ -5,12 +5,31 @@ export class EditBugDialogPage {
   readonly deleteButton: Locator;
   readonly cancelButton: Locator;
   readonly saveButton: Locator;
+  readonly closeButton: Locator;
+  readonly idField: Locator;
+  readonly titleField: Locator;
+  readonly severityField: Locator;
+  readonly stateField: Locator;
+  readonly ownerField: Locator;
+  readonly descriptionField: Locator;
 
   constructor(page: Page) {
     this.dialog = page.getByRole("dialog", { name: /Edit bug/ });
     this.deleteButton = this.dialog.getByRole("button", { name: "Delete" });
     this.cancelButton = this.dialog.getByRole("button", { name: "Cancel" });
     this.saveButton = this.dialog.getByRole("button", { name: "Save" });
+    this.closeButton = this.dialog.getByRole("button", { name: "Close" });
+    this.idField = this.dialog.getByLabel("ID");
+    this.titleField = this.dialog.getByLabel("Title");
+    this.severityField = this.dialog.getByLabel("Severity");
+    this.stateField = this.dialog.getByLabel("State");
+    this.ownerField = this.dialog.getByLabel("Owner");
+    this.descriptionField = this.dialog.getByLabel("Description");
+  }
+
+  /** Clicks the dimmed backdrop outside the dialog panel bounds. */
+  async clickBackdrop() {
+    await this.dialog.click({ position: { x: 4, y: 4 } });
   }
 
   async expectVisible() {
